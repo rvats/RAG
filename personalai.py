4f545406f8b63098da5820f242979261b5bee090
Rahul Vats <vats.rahul@gmail.com>
2024-07-23 13:10:25 -0500

    Integrating Speech Pieces

--- personalai.py
+++ personalai.py
@@ -4,17 +4,15 @@ import pyttsx3
 
 openai.api_key = constants.APIKEY 
 
-message = {"role":"user", "content": input("This is the beginning of your chat with AI. [To exit, send \"###\".]\n\nYou:")};
+message = {"role":"user", "content": input("This is the beginning of your chat with AI. [To exit, send \"###\".]\n\nYou: ")};
 conversation = [{"role": "system", "content": "DIRECTIVE_FOR_gpt-4o"}]
 engine = pyttsx3.init()
 
 while(message["content"]!="###"):
     conversation.append(message)
     completion = openai.ChatCompletion.create(model="gpt-4o", messages=conversation) 
-    message["content"] = input(f"TWI: {completion.choices[0].message.content} \nYou: ")
-    print()
-    conversation.append(completion.choices[0].message)
-    engine.say(completion.choices[0].message)
+    print(f"TWI: {completion.choices[0].message.content}")
+    engine.say(completion.choices[0].message.content)
     engine.runAndWait()
-
-
+    message["content"] = input("You: ")
+    conversation.append(completion.choices[0].message)
