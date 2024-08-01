8b2efaf9403bfa1f8d0771ba00823e305ce5abeb
Rahul Vats <vats.rahul@gmail.com>
2024-07-31 22:05:54 -0700

    Add files via upload

--- voice.py
+++ voice.py
@@ -60,6 +60,7 @@ def voice_to_text():
 
 def sayFunc(phrase):
     engine = pyttsx3.init()
+    engine.setProperty('voice', r'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0')
     engine.say(phrase)
     engine.runAndWait()
 
